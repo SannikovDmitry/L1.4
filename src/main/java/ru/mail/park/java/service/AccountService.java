0f16c521fdb7efdd6a6508d3b90623661e2c7013
Dmitry Sannikov <sdk96@mail.ru>
2016-10-03 23:55:11 +0300

    Added autorization

--- src/main/java/ru/mail/park/java/service/AccountService.java
+++ src/main/java/ru/mail/park/java/service/AccountService.java
@@ -3,7 +3,7 @@ package ru.mail.park.java.service;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import ru.mail.park.model.UserProfile;
+import ru.mail.park.java.model.UserProfile;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +11,19 @@ import java.util.Map;
 /**
  * Created by Дмитрий on 03.10.2016.
  */
+
+@Service
 public class AccountService {
+    private Map<String, UserProfile> userNameToUser = new HashMap<>();
+
+    public UserProfile addUser(String login, String password, String email) {
+        final UserProfile userProfile = new UserProfile(login, email, password);
+        userNameToUser.put(login, userProfile);
+        return userProfile;
+    }
+
+    public UserProfile getUser(String login) {
+        return userNameToUser.get(login);
+    }
 
 }
